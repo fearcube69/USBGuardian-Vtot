2e930477a6a46de8fba5f8c9ca6d627e8c4628cc
neiru aonuma <63183005+fearcube69@users.noreply.github.com>
2023-11-14 09:40:27 +0800

    update

--- insv4.sh
+++ insv4.sh
@@ -22,7 +22,7 @@ read -s -p "Enter password for 'securite' user: " SECURITE_PASSWORD &&
 echo "securite:$SECURITE_PASSWORD" | sudo chpasswd &&
 echo "securite ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers &&
 sudo usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi securite
-
+su - securite
 # Delete 'pi' account
 sudo pkill -u pi
 sudo deluser --remove-home pi
