600fd0ce556dc5feaf28db31737d00bb96d75269
neiru aonuma <63183005+fearcube69@users.noreply.github.com>
2023-12-05 14:41:17 +0800

    comment

--- USBGuardian-GUI/details.cpp
+++ USBGuardian-GUI/details.cpp
@@ -1,28 +1,38 @@
+// Include the necessary header files
 #include "details.h"
 #include "ui_details.h"
 #include <QDebug>
 
+// Constructor for the Details class
 Details::Details(const QString &report, QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::Details)
+   QDialog(parent), // Inherit from QDialog
+   ui(new Ui::Details) // Initialize the UI
 {
-    ui->setupUi(this);
-    setWindowFlags(Qt::Dialog | Qt::WindowMinimizeButtonHint | Qt::WindowMaximizeButtonHint);
+   // Set up the UI
+   ui->setupUi(this);
 
-    ui->buttonBox->clear();
-    ui->buttonBox->addButton(QDialogButtonBox::Close);
+   // Set the window flags
+   setWindowFlags(Qt::Dialog | Qt::WindowMinimizeButtonHint | Qt::WindowMaximizeButtonHint);
 
-    QStringList virusesList = report.split('\n', QString::SkipEmptyParts);
+   // Clear the button box and add a close button
+   ui->buttonBox->clear();
+   ui->buttonBox->addButton(QDialogButtonBox::Close);
 
-    foreach (QString virus, virusesList) {
-        if (virus.contains("FOUND") || virus.contains("Removed")) {
-            QListWidgetItem *item = new QListWidgetItem(virus.replace("FOUND", "INFECTED"));
-            ui->listDetails->addItem(item);
-        }
-    }
+   // Split the report into a list of viruses
+   QStringList virusesList = report.split('\n', QString::SkipEmptyParts);
+
+   // For each virus in the list, if the virus is found or removed, add it to the list widget
+   foreach (QString virus, virusesList) {
+       if (virus.contains("FOUND") || virus.contains("Removed")) {
+           QListWidgetItem *item = new QListWidgetItem(virus.replace("FOUND", "INFECTED"));
+           ui->listDetails->addItem(item);
+       }
+   }
 }
 
+// Destructor for the Details class
 Details::~Details()
 {
-    delete ui;
+   // Delete the UI object to free the memory
+   delete ui;
 }
